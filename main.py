f077f1a41a216a0571dcf57c793c7d90310129ef
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-05-12 17:02:49 +0330

    Update main.py module

--- main.py
+++ main.py
@@ -1,6 +1,87 @@
 from getpass import getpass
 
 
+def user_register():
+    """
+
+    :return:
+    """
+
+    print('Register.')
+    username = input('username: ')
+    phone_number = input('phone_number: ')
+    password = getpass('password: ')
+
+    # do some things for register.
+
+
+def user_profile():
+    """
+    
+    :return: 
+    """
+    pass
+
+
+def update_user_profile():
+    """
+    
+    :return: 
+    """
+    pass
+
+
+def change_user_password():
+    """
+
+    :return:
+    """
+
+    print('Change password.')
+    old_password = getpass('Old password: ')
+    new_password = getpass('New password: ')
+    confirm_password = getpass('Confirm password: ')
+
+    # do something
+
+
+def logout():
+    """
+
+    :return:
+    """
+
+    pass
+
+
+def user_login():
+    """
+
+    :return:
+    """
+
+    print('Login')
+    username = input('username: ')
+    password = getpass('password: ')
+
+    # if err
+    #     raise err
+
+    print('1: Profile.  2: Edit profile.  3: Change password.  4: Logout.')
+    
+    register_inp = input('Yor choice: ')
+    
+    match register_inp:
+        case '1':
+            user_profile()
+        case '2':
+            update_user_profile()
+        case '3':
+            change_user_password()
+        case '4':
+            logout()
+
+
 def main():
     """
 
@@ -14,38 +95,9 @@ def main():
             case '0':
                 break
             case '1':
-                print('Register.')
-                username = input('username: ')
-                phone_number = input('phone_number: ')
-                password = getpass('password: ')
-                # do some things for register.
+                user_register()
             case '2':
-                print('Login')
-                username = input('username: ')
-                password = getpass('password: ')
-                # if err
-                #     raise err
-                print('1: Profile.  2: Edit profile.  3: Change password.  4: Logout.')
-                register_inp = input('Yor choice: ')
-                match register_inp:
-                    case '1':
-                        pass
-                        # show data
-                        break
-                    case '2':
-                        pass
-                        # edit profile
-                        break
-                    case '3':
-                        pass
-                        print('Change password.')
-                        old_password = getpass('Old password: ')
-                        new_password = getpass('New password: ')
-                        confirm_password = getpass('Confirm password: ')
-                        # do something
-                    case '4':
-                        break
-                        # logout
+                user_login()
 
 
 if __name__ == '__main__':
