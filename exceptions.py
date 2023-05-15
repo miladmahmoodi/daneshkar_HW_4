1b9ed367ebaf740fe7528160812451832f933e18
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-05-15 13:30:41 +0330

    Add dovstring to module and classes.

--- exceptions.py
+++ exceptions.py
@@ -1,20 +1,32 @@
 """
-
+This module defines custom exception classes for a user authentication system.
 """
 
 
 class ExistsUserError(Exception):
+    """
+    Raised when a user with the given username already exists.
+    """
     pass
 
 
 class SigninError(Exception):
+    """
+    Raised when there is an error during the sign-in process.
+    """
     pass
 
 
 class PasswordError(Exception):
+    """
+    Raised when the password provided is invalid.
+    """
     pass
 
 
 class ConfirmPasswordError(Exception):
+    """
+    Raised when the passwords don`t match.
+    """
     pass
 
