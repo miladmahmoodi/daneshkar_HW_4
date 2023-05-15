23025947eff71d264066e00dd3162eac8c909a2b
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-05-15 10:28:13 +0330

    Update type annotations and docstring module and functions.

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+"""
+This module is used for User module utilities.
+"""
+
 import json, re
 
 from uuid import uuid4
@@ -8,15 +12,28 @@ from hashlib import sha256
 
 
 class Utils:
+    """
+    A class representing utility functions for the User module.
+    """
+
     @staticmethod
-    def id_generator():
+    def id_generator() -> int:
+        """
+        Generate a unique id using the uuid4 module.
+
+        :return: An integer representing the unique id.
+        """
         return uuid4().int
 
     @staticmethod
     def check_password(password: str) -> str:
         """
+        Hash the given password using the sha256 algorithm.
 
+        :param password: A string representing the password to be hashed.
+        :return: A string representing the hashed password.
         """
+
         if not Utils.is_valid_password(password):
             raise Exception('The password must contain uppercase and lowercase letters, numbers and special symbols'
                             ' and must have at least 4 characters.')
@@ -25,8 +42,19 @@ class Utils:
     @staticmethod
     def is_valid_password(password: str) -> bool:
         """
+        Check whether the given password is valid or not.
 
+        The password must meet the following requirements:
+        - At least 4 characters long
+        - Contains at least one uppercase letter
+        - Contains at least one lowercase letter
+        - Contains at least one digit
+        - Contains at least one special character from the set @#$%^&+=!
+
+        :param password: A string representing the password to be checked.
+        :return: True if the password is valid, False otherwise.
         """
+
         pattern = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{4,}$"
         if re.match(pattern, password):
             return True
@@ -35,9 +63,10 @@ class Utils:
     @staticmethod
     def hashing_password(password: str) -> str:
         """
+        Hashes the given password using the SHA-256 algorithm.
 
-        :param password:
-        :return:
+        :param password: A string representing the password to be hashed.
+        :return: A string representing the hashed password.
         """
 
         hash_pass = sha256(
@@ -47,10 +76,14 @@ class Utils:
         return hash_pass
 
     @staticmethod
-    def save_data(data: list):
+    def save_data(data: list) -> None:
+        """
+        Saves the given data to a JSON file named 'database.json'.
+
+        :param data: A list representing the data to be saved.
+        :return: None.
         """
 
-        """
         with open('database.json', 'a', encoding='utf-8') as file:
             json.dumps(
                 data
@@ -59,8 +92,13 @@ class Utils:
     @staticmethod
     def match_password(password: str, other: str) -> bool:
         """
+        Check whether the given password and other match or not.
 
+        :param password: A string representing the first password.
+        :param other: A string representing the second password to compare with the first one.
+        :return: True if the passwords match, False otherwise.
         """
+
         if password == other:
             return True
         return False
