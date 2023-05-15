d2071dbbbdbbfd7a4c24508c3f7d735f966fb6e4
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-05-15 13:04:02 +0330

    Add ExistsUserError, SigninError, PasswordError,ConfirmPasswordError exceptions

---
+++ exceptions.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+
+
+class ExistsUserError(Exception):
+    pass
+
+
+class SigninError(Exception):
+    pass
+
+
+class PasswordError(Exception):
+    pass
+
+
+class ConfirmPasswordError(Exception):
+    pass
+
